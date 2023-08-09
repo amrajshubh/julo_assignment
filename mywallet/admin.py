82e57e144c7abbd13ed9d2d6270dedd70b8ec4f2
shubham raj <shubham.raj@renewbuy.com>
2023-08-09 23:09:38 +0530

    admin changes

--- mywallet/admin.py
+++ mywallet/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from mywallet.models import *
 
 # Register your models here.
+admin.site.register(Wallet)
+admin.site.register(Customer)
+admin.site.register(Transactions)
--- mywallet/models.py
+++ mywallet/models.py
@@ -15,12 +15,18 @@ class Customer(TimeStamp):
     customer_xid = models.UUIDField(primary_key=True, default=uuid.uuid4)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.user.username
+
 
 class Wallet(TimeStamp):
     id = models.UUIDField(primary_key=True,  default=uuid.uuid4)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, unique=True)
     is_active = models.BooleanField(default=False)
     balance = models.IntegerField(default=0, null=True)
+
+    def __str__(self):
+        return self.id
 
 class Transactions(TimeStamp):
     StatusChoices = (
@@ -36,3 +42,6 @@ class Transactions(TimeStamp):
     status = models.CharField(max_length=20, choices=StatusChoices, default="pending")
     transaction_type = models.CharField(max_length=50, choices=TransactionChoices)
     wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.id
